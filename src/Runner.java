a8fb198117095d5d2d3b1b460fd9c666eafe9abf
denisoetken <denisoetken@hotmail.de>
2016-11-10 20:35:03 +0100

    Parkhaus

--- src/Runner.java
+++ src/Runner.java
@@ -9,7 +9,7 @@ public class Runner {
         }
         while (true) {
             try {
-                Thread.sleep(100);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
